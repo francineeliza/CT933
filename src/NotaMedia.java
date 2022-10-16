01066a2928a591e04694ef4ef70250480c4a96e1
Francine Cavalcante <francine.eliza@gmail.com>
2022-10-15 22:51:03 -0300

    Tirando espaços em branco.

--- src/NotaMedia.java
+++ src/NotaMedia.java
@@ -5,10 +5,10 @@ public class NotaMedia {
         int[] notas = new int[4];
         int i = 0;
         Scanner sc = new Scanner(System.in);
-        while (i < 4){
+        while (i < 4) {
             System.out.println("Informe a nota da prova " + (i + 1));
             int nota = sc.nextInt();
-            if (nota < 0 || nota > 10){
+            if (nota < 0 || nota > 10) {
                 System.out.println("Nota inválida");
                 return;
             }
@@ -18,7 +18,5 @@ public class NotaMedia {
         sc.close();
         double media = (notas[0] + notas[1] + notas[2] + notas[3]) / 4.0;
         System.out.printf("A média do aluno é %.2f", media);
-
-
     }
 }
