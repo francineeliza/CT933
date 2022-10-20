207e683b8ab568bb37d04b3e41b76d9f901c34cd
Francine Cavalcante <francine.eliza@gmail.com>
2022-10-20 18:44:44 -0300

    Adicionando print dos totais.

--- src/TabelaPrice.java
+++ src/TabelaPrice.java
@@ -11,13 +11,18 @@ public class TabelaPrice {
         System.out.println("Informe o tempo para pagamento:");
         int tempoPagamento = sc.nextInt();
         double saldoDevedor = valorEmprestimo;
-
+        double totalJuros = 0;
+        double totalAmortizacao = 0;
         double prestacao = valorEmprestimo * (Math.pow((1 + taxaJuros), tempoPagamento) * taxaJuros) / (Math.pow((1 + taxaJuros), tempoPagamento) - 1);
+        double totalPrestacao = prestacao * tempoPagamento;
+
         System.out.printf("Valor fixo da parcela R$ %.2f, Saldo devedor total R$ %.2f%n", prestacao, valorEmprestimo);
 
         for (int i = 0; i < tempoPagamento; i++) {
             double juros = saldoDevedor * taxaJuros;
             double amortizacao = prestacao - juros;
+            totalJuros = juros + totalJuros;
+            totalAmortizacao = amortizacao + totalAmortizacao;
             saldoDevedor = saldoDevedor - amortizacao;
 
             if (saldoDevedor < 0.01) {
@@ -25,6 +30,7 @@ public class TabelaPrice {
             }
             System.out.printf("Parcela %d | Juros: R$ %.2f | Amortização: R$ %.2f | Saldo devedor: R$ %.2f%n", i + 1, juros, amortizacao, saldoDevedor);
         }
+        System.out.printf("Total: Prestação %.2f, Juros R$ %.2f, Amortização R$ %.2f", totalPrestacao, totalJuros, totalAmortizacao);
         sc.close();
     }
 }
